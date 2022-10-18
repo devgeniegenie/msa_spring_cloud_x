636fea5afc67b84e54a30c2d410015f214d72f91
Genie <devgeniegenie@gmail.com>
2022-10-18 21:16:52 +0900

    api gateway service filter java에서 구현 : yml -> @Configuration

--- API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,14 +1,23 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //@Controller와 차이 : req body, res body 구현할지 가져다쓸지의 차이
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the first service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "In First Service";
     }
 }
--- API Gateway Service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ API Gateway Service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -2,6 +2,7 @@ package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +15,9 @@ public class SecondServiceController {
         return "Welcome to the Second service.";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "In Second Service";
+    }
 }
