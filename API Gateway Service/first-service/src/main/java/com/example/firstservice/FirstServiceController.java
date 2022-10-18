9261d01bb5bb8f82ed76f0f9e361f46120c144bd
Genie <devgeniegenie@gmail.com>
2022-10-18 20:43:19 +0900

    API Gateway Service - zuul > spring cloud gateway로 변경

--- API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //@Controller와 차이 : req body, res body 구현할지 가져다쓸지의 차이
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
--- API Gateway Service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ API Gateway Service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -2,12 +2,11 @@ package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
     @GetMapping("/welcome")
