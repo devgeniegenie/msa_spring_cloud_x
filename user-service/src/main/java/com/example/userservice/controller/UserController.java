08590dbe9722b20868fc7cae4a230972991ca2ee
Genie <devgeniegenie@gmail.com>
2022-10-24 22:00:04 +0900

    env value 사용설정, h2 test

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private Environment env;
+
+    private final Greeting greeting;
+
+    @Autowired
+    public UserController(Environment env, Greeting greeting) {
+        this.env = env;
+        this.greeting = greeting;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It`s Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+}
